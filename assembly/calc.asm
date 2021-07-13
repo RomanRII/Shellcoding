1ae0dd05372e970bdfbfca0dc8a0f9b3fa3675e0
RomanRII <74742067+RomanRII@users.noreply.github.com>
2021-07-13 11:09:46 -0700

    Update calc.asm

--- assembly/calc.asm
+++ assembly/calc.asm
@@ -1,6 +1,8 @@
 ; Name: Basic calc pop
 ; Author: RomanRII
 ; Date: 07/13/21
+; nasm -f bin calc.asm -o Bins/cmd.bin
+; cat Bins/cmd.bin | base64 & copy that into a b64 shell runner
 
 BITS 32
 section .text
